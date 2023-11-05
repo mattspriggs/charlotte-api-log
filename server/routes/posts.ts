8beca75ae30ad27f6fb5439a30837b4e77649458
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-06 11:32:14 +1300

    Initial commit

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -2,4 +2,9 @@ import express from 'express'
 
 const router = express.Router()
 
+// GET all posts
+router.get('/', async (req, res) => {
+  const id = Number(req.params.id)
+})
+
 export default router
