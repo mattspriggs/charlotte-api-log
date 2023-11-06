a4bb65c589cb5d3d21320c770cf2e00473c61439
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-06 14:36:31 +1300

    Get all posts working

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,3 +1,15 @@
 import connection from './connection.ts'
+import { Post, PostData, PostUpdate } from '../../models/post.ts'
 
-export function getAllPosts(db = connection) {}
+export function getAllPosts(db = connection) {
+  return db('posts').select(
+    'id',
+    'title',
+    'date_created as dateCreated',
+    'text'
+  )
+}
+
+export function addPost(post: Post): Promise<PostData[]> {
+  return connection('posts').insert(post).returning('*')
+}
--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,10 +1,19 @@
 import express from 'express'
+import * as db from '../db/db'
+import { Post } from '../../models/post'
 
 const router = express.Router()
 
 // GET all posts
 router.get('/', async (req, res) => {
-  const id = Number(req.params.id)
+  try {
+    const posts: Post = await db.getAllPosts()
+    res.json(posts)
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred while getting posts',
+    })
+  }
 })
 
 export default router
