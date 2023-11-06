e14493083987e3a1d05134e61184b8400c0597ac
Matt Spriggs <matt.c.spriggs@gmail.com>
2023-11-06 20:30:38 +1300

    Added onDelete('cascade') to migration for comments

--- server/db/migrations/20170517194500_comments.js
+++ server/db/migrations/20170517194500_comments.js
@@ -1,7 +1,7 @@
 export async function up(knex) {
   await knex.schema.createTable('Comments', (table) => {
     table.increments().primary()
-    table.integer('post_id').references('Posts.id')
+    table.integer('post_id').references('Posts.id').onDelete('cascade')
     table.date('date_posted')
     table.string('comment')
   })
